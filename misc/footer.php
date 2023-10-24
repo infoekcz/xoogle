5340dce75550f9c27224aa07eebd140893f9f2b1
infoekcz <114327090+infoekcz@users.noreply.github.com>
2023-10-24 19:58:05 +0200

    Update footer.php

--- misc/footer.php
+++ misc/footer.php
@@ -4,7 +4,8 @@
 	<a href="https://blog.xoogle.cz/mobilni-aplikace-xoogle-2022/">>Xoogle aplikace</a>
 	<a href="./settings.php">>Nastavení</a>
 	<a href="https://blog.xoogle.cz/dostupne-instance-librex-2022/">>Instance</a>
-    <a href="https://blog.xoogle.cz/podpora-2022/">>Podpora❤️</a>
+   	<a href="https://blog.xoogle.cz/podpora-2022/">>Podpora❤️</a>
+	<a href="https://github.com/infoekcz/xoogle/">>Zdroják</a>
 </div>
 <div class="git-container">
     <?php
